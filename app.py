c8df2b7cc4a4474e8bf7d5647f24c947f4b88d75
Derry Birkett <derry@d2tstudio.com>
2015-05-25 10:31:01 +0200

    Add new route

--- app.py
+++ app.py
@@ -7,14 +7,26 @@ def create_app(configfile=None):
 
     # MOVE THESE TO ENVIRONMENT
     inedicola.config['DEBUG'] = 'true'
+    inedicola.config['STATIC_URL'] = '/static'
 
     # ADD EXTENSIONS
     inedicola.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
+    # DEFAULTS
+    defaults = {
+        'STATIC_URL': inedicola.config.get('STATIC_URL', '/static/'),
+    } 
+
     # ROUTING
     @inedicola.route('/')
     def index():
-        return render_template('components.jade')
+        opts = defaults.copy()
+        return render_template('components.jade', **opts)
+
+    @inedicola.route('/options')
+    def options():
+        opts = defaults.copy()
+        return render_template('options.jade', **opts)
 
     return inedicola
 
